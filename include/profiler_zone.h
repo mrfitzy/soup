42b54d4633bbb6d833e09194b50c4f7ecc2083fd
mrfitzy <fitzz86@gmail.com>
2026-09-18 12:23:30 -0400

    fix profile-less build

--- include/profiler_zone.h
+++ include/profiler_zone.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#ifndef TRACY_ENABLE
+
+#define PROFILER_ZONE_BEGIN(ctx, name)
+#define PROFILER_ZONE_END(ctx, name)
+
+#else
+
 #include <tracy/TracyC.h>
-
 #define PROFILER_ZONE_BEGIN(ctx, name) TracyCZoneN(ctx, name, true)
-
 #define PROFILER_ZONE_END(ctx, name) TracyCZoneEnd(ctx)
+
+#endif // TRACY_ENABLE
